e7c9f6070f2845c10fddd95d78e65936f3cbad99
Leandro <lele.alvarez86@hotmail.com>
2023-07-26 01:47:56 -0300

    First Commit

---
+++ static/assets/js/scripts.js
@@ -0,0 +1,145 @@
+/* Description: Custom JS file */
+
+
+(function($) {
+    "use strict"; 
+	
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+    
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    // hover in desktop mode
+    function toggleDropdown (e) {
+        const _d = $(e.target).closest('.dropdown'),
+            _m = $('.dropdown-menu', _d);
+        setTimeout(function(){
+            const shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _m.toggleClass('show', shouldOpen);
+            _d.toggleClass('show', shouldOpen);
+            $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+        }, e.type === 'mouseleave' ? 300 : 0);
+    }
+    $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
+
+
+/* About animation */
+let sections = document.querySelectorAll('section');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
+        }
+    })
+}
+
+/* skills animation */
+
+//let progress-bars = document.querySelectorAll('progress-bars');
+//window.onscroll = () => {
+//    sections.forEach(sec => {
+//        let top = window.scrollY;
+//        let offset = sec.offsetTop -50;
+//        let height = sec.offsetHeight;
+//
+//        if (top >= offset && top < offset + height) {
+//            sec.classList.add('show-animate');
+//        }
+//        else {
+//            sec.classList.remove('show-animate');
+//        }
+//    })
+//}
+
+
+/* skills animation 2 */
+
+//const skillsSection = document.getElementById('skills');
+//
+//const progressBars = document.querySelectorAll('.progress-bar');
+//
+//function showProgress(){
+//    progressBars.forEach(progressBars=> {
+//    progressBars.style.opacity = 1;
+//
+//    });
+//}
+
+//function hideProgress(){
+//    progressBars.forEach(progressBars => {
+//    progressBars.style.opacity = 0;
+//    });
+
+
+//window.addEventListener('scroll',() => {
+//    const sectionPos = skillsSection.getBoundingClientRect().top;
+//    const screenPos = window.innerHeight;
+//
+//    if (sectionPos < screenPos){
+//        showProgress();
+//    }else{
+//        hideProgress();
+//    }
+//});
